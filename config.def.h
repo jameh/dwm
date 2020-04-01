d3d90e571631d1a901c4ecdb0448ca65cf0e7734
Jamie Macdonald <jamie.alban@gmail.com>
2020-04-01 10:56:09 -0700

    Add tag rules for programs.

--- config.def.h
+++ config.def.h
@@ -25,16 +25,20 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1:dev", "2:web", "3:media", "4:music", "5:org", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class             instance    title              tags mask     isfloating   monitor */
+	{ "Gimp",            NULL,       NULL,              0,            1,           -1 },
+	{ "Firefox",         NULL,       NULL,              1 << 1,       0,           -1 },
+	{ "Chromium",        NULL,       NULL,              1 << 1,       0,           -1 },
+	{ "Spotify",         NULL,       NULL,              1 << 3,       0,           -1 },
+	{ "Show-splash-gtk", NULL,       NULL,              1 << 3,       0,           -1 },
+	{ "Bitwig Studio",   NULL,       NULL,              1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
