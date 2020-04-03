b6f05213ec8d5dcf90c031ed8dd745942e0f8f33
Jamie Macdonald <jamie.alban@gmail.com>
2020-04-02 22:16:18 -0700

    testing change

--- dwm-feature-add.sh
+++ dwm-feature-add.sh
@@ -1,53 +1,103 @@
 #!/bin/sh
 
-patches_url="https://dwm.suckless.org/patches/$1/"
+PATCHES_URL="https://dwm.suckless.org/patches/$1/"
 
-[ -n "$DWM_GIT_DIR" ] && cd $DWM_GIT_DIR
+[ -n "$DWM_GIT_DIR" ] && cd "$DWM_GIT_DIR"
 
-git checkout master > /dev/null
-[ $? -ne 0 ] && exit $?
+exit_on_fail() {
+	exit_code="$?"
+	[ "$exit_code" -eq 0 ] && return 0
+	[ -n "$2" ] && exit_code="$2"
 
-git status | grep "Changes not staged for commit:" > /dev/null
-[ $? -eq 0 ] && echo "Unstaged changes exist. Exiting." && exit $?
+	[ -n "$3" ] && error_handler="$3"
+	echo $@
+	[ $("$error_handler" "$exit_code") -eq 0 ] && return 0
 
-echo "Downloading patch list from $patches_url:"
-patches_html=$(curl "$patches_url")
-exit_code=$?
-[ "$exit_code" -ne 0 ] && echo "Download error. Exiting." && exit "$exit_code"
+	exit_msg="$1"
+	[ -n "$exit_msg" ] && echo "$exit_msg"
+	echo "Exiting."
+	exit "$exit_code"
+}
 
-patches_list=$(echo "$patches_html" | pup 'div#main li > a attr{href}')
-[ -z "$patches_list" ] && echo "No patches found at $patches_url" && exit 1
+check_git() {
+	git status &> /dev/null
+	exit_on_fail "Not in a git repository."
 
-# determine which patch is wanted
-n_lines=$(echo "$patches_list" | wc -l)
-echo ""
-echo "Found $n_lines patches:"
-echo "$patches_list" | awk '{print NR ": " $1 }'
-echo ""
-read -p "selection[1..$n_lines]: " selection
-selected_patch=$(echo "$patches_list" | head -$selection | tail -1)
+	[ -z "$(git status | grep "Changes not staged for commit:")" ]
+	exit_on_fail "Unstaged changes exist."
+}
 
-# download patch
-patch_url="$patches_url/$selected_patch"
-echo "Downloading $patch_url"
-patch=$(curl "$patch_url")
+get_patch_list() {
+	echo "Downloading patch list from $PATCHES_URL:"
 
-revision=$(echo "$selected_patch" | sed 's/.diff$//' | rev | cut -d '-' -f1 | rev)
-branch_name=$(echo "$selected_patch" | sed 's/^dwm-*//' | sed 's/.diff$//')
+	_patches_html=$(curl "$PATCHES_URL")
+	exit_on_fail "Download error."
 
-git checkout "$revision" 2> /dev/null
-[ $? -ne 0 ] && exit $?
+	_patches_list=$(echo "$_patches_html" | pup 'div#main ul:first-of-type li > a attr{href}')
+	[ -n "$_patches_list" ]
+	exit_on_fail "No patches found at $PATCHES_URL"
 
-git checkout -b "$branch_name" > /dev/null
-if [ $? -eq 128 ]; then
-	read -p "delete existing branch $branch_name? [n] " delete
-	[ "$delete" != "y" ] && [ "$delete" != "Y" ] && echo "Exiting." && exit 1
-fi
+	return "$_patches_list"
+}
 
-git branch -D "$branch_name"
+select_patch() {
+	_patches_list="$1"
+	_n_lines=$(echo "$patches_list" | wc -l)
+	echo ""
+	echo "Found $_n_lines patches:"
+	echo "$_patches_list" | awk '{print NR ": " $1 }'
+	echo ""
+	read -p "selection[1..$_n_lines]: " _selection
+	return $(echo "$_patches_list" | head -$_selection | tail -1)
+}
+
+download_patch() {
+	_patch_url="$PATCHES_URL/$1"
+	echo "Downloading $_patch_url"
+	return $(curl "$_patch_url")
+}
+
+get_base_revision() {
+	return $(echo "$1" | sed 's/.diff$//' | rev | cut -d '-' -f1 | rev)
+}
+
+get_feature_branch_name() {
+	return $(echo "$1" | sed 's/^dwm-*//' | sed 's/.diff$//')
+}
+
+checkout_handler() {
+	if [ $1 -eq 128 ]; then
+		[ -z $2 ] && return 128
+		_branch_name=$2
+
+		read -p "delete existing branch $_branch_name? [n] " _delete
+		if [ "$_delete" = "y" ] && [ "$_delete" = "Y" ]; then
+			echo "Not deleting $_branch_name"
+			return 128
+		else
+			git branch -D "$_branch_name"
+			git checkout -b "$_branch_name"
+			return 0
+		fi
+	fi
+}
+
+check_git
+
+patch_name=$(select_patch $(get_patch_list))
+patch=$(download_patch "$patch_name")
+revision=$(get_base_revision "$patch_name")
+
+git checkout "$revision"
+exit_on_fail "Could not checkout $revision"
+
+branch_name=$(get_feature_branch_name "$patch_name")
+
 git checkout -b "$branch_name"
+exit_on_fail "Could not check out $branch_name" 128 checkout_handler "$branch_name"
+
 
 echo "$patch" | patch -p1
 
-commit_message=$(printf "Apply patch %s.\n\n%s\n" $selected_patch $patches_url)
+commit_message=$(printf "Apply patch %s.\n\n%s\n" $patch_name $PATCHES_URL)
 git commit -am "$commit_message"
